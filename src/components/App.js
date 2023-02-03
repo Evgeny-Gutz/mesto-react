850ef7cf965e53eff124572257163716712b07f8
Evgeny Gutz <evgeny.gutz@yandex.ru>
2023-02-03 18:09:43 +0300

    refactor: Изменить передачу инпутов в PopupWithForm, применить children и деструктуризацию

--- src/components/App.js
+++ src/components/App.js
@@ -63,17 +63,40 @@ function App() {
             name="profile" 
             title="Редактировать профиль" 
             isOpen={isEditProfilePopupOpen && "popup_opened"} 
-            onClose={closeAllPopups} />
+            onClose={closeAllPopups}>
+                <label className="popup__field">
+                    <input id="name-input" className="popup__input popup__input_type_name" type="text" name="name" minLength="2" maxLength="40" required />
+                    <span className="name-input-error popup__input-error"></span>
+                </label>
+                <label className="popup__field">
+                    <input id="profession-input" className="popup__input popup__input_type_job" type="text" name="profession" minLength="2" maxLength="200" required />
+                    <span className="profession-input-error popup__input-error"></span>
+                </label>
+                <input className="popup__submit" type="submit" name="submit" value="Сохранить"></input></PopupWithForm>
         <PopupWithForm 
             name="card" 
             title="Новое место" 
             isOpen={isAddPlacePopupOpen && "popup_opened"} 
-            onClose={closeAllPopups} />
+            onClose={closeAllPopups}>
+                <label className="popup__field">
+                     <input id="title-input" className="popup__input popup__input_type_title" type="text" name="title" placeholder="Название" minLength="2" maxLength="30" required />
+                     <span className="title-input-error popup__input-error"></span>
+                 </label>
+                 <label className="popup__field">
+                     <input id="link-input" className="popup__input popup__input_type_link" type="url" name="link" placeholder="Ссылка на картинку" />
+                     <span className="link-input-error popup__input-error"></span>
+                 </label>
+                 <input className="popup__submit" type="submit" name="submit" value="Создать"></input></PopupWithForm> 
         <PopupWithForm 
             name="avatar" 
             title="Обновить аватар" 
             isOpen={isEditAvatarPopupOpen && "popup_opened"} 
-            onClose={closeAllPopups} />
+            onClose={closeAllPopups}>
+                <label className="popup__field">
+                     <input id="link-input-avatar" className="popup__input popup__input_type_link" type="url" name="link" placeholder="Ссылка на картинку" required />
+                     <span className="link-input-avatar-error popup__input-error"></span>
+                 </label>
+                 <input className="popup__submit" type="submit" name="submit" value="Сохранить"></input></PopupWithForm>
         <ImagePopup
             onOpen={handleCardClick}
             card={selectedCard}
