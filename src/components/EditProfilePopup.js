1983837ec2938a7633a872126eff274aa6693667
Evgeny Gutz <evgeny.gutz@yandex.ru>
2023-02-09 22:07:08 +0300

    refactor: Добавте управляемые компоненты. Используйте значение по умолчанию из curentUser

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,22 +1,43 @@
+import React from "react";
+import {useState, useEffect, useContext} from "react";
+import { UserContext } from "../contexts/CurrentUserContext";
 import PopupWithForm from "./PopupWithForm";
 
 function EditProfilePopup({isOpen, onClose}) {
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const currentUser = useContext(UserContext);
+
+    function handleChangeName(e) {
+        setName(e.target.value);
+    }
+
+    function handleChangeDescription(e) {
+        setDescription(e.target.value);
+    }
+
+    useEffect(() => {
+        setName(currentUser.name);
+        setDescription(currentUser.about)
+    }, [currentUser]);
+
     return (
         <PopupWithForm 
             name="profile" 
             title="Редактировать профиль" 
             isOpen={isOpen && "popup_opened"} 
             onClose={onClose}>
-                <label className="popup__field">
-                    <input id="name-input" className="popup__input popup__input_type_name" type="text" name="name" minLength="2" maxLength="40" required />
+                { name && description && <><label className="popup__field">
+                    <input id="name-input" className="popup__input popup__input_type_name" type="text" name="name" value={name} onChange={handleChangeName} minLength="2" maxLength="40" required />
                     <span className="name-input-error popup__input-error"></span>
                 </label>
                 <label className="popup__field">
-                    <input id="profession-input" className="popup__input popup__input_type_job" type="text" name="profession" minLength="2" maxLength="200" required />
+                    <input id="profession-input" className="popup__input popup__input_type_job" type="text" name="name" value={description} onChange={handleChangeDescription} minLength="2" maxLength="200" required />
                     <span className="profession-input-error popup__input-error"></span>
                 </label>
-                <input className="popup__submit" type="submit" name="submit" value="Сохранить"></input>
+                <input className="popup__submit" type="submit" name="submit" value="Сохранить"></input></>}
         </PopupWithForm>
+        
     );
 }
 
