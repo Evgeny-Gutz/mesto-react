4f6d64abcf7dc2a6d0fd6f4bcbd926f83f8e8f7d
Evgeny Gutz <evgeny.gutz@yandex.ru>
2023-02-09 15:12:03 +0300

    refactor: Создайте глобальный стейт, подпишите на него Main, удаить лишние стейт переменные

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import api from "../utils/Api";
+import {UserContext} from '../contexts/CurrentUserContext.js';
 import PopupWithForm from './PopupWithForm';
 import ImagePopup from './ImagePopup';
 
@@ -10,6 +12,7 @@ function App() {
     const [isAddPlacePopupOpen, setAddPlacePopupOpen] = React.useState(false);
     const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = React.useState(false);
     const [selectedCard, setSelectedCard] = React.useState({isVisible:false, name: "", link: ""});
+    const [currentUser, setCurrentUser] = React.useState({});
 
     function closeAllPopups () {
         setEditAvatarPopupOpen(false);
@@ -29,6 +32,11 @@ function App() {
     function handleBackgroundClose(evt) {
         if(evt.target.classList.contains('popup_opened')) closeAllPopups();
     }
+
+    React.useEffect(() => {
+        api.getDataUser()
+            .then((res) => {setCurrentUser(res)});
+    }, []);
     
     React.useEffect( () => {
         document.addEventListener('keydown', handleEscClose);
@@ -51,7 +59,7 @@ function App() {
     }
 
     return (
-    <>
+    <UserContext.Provider value={currentUser}>
         <Header />
         <Main 
             onEditProfile={handleEditProfileClick} 
@@ -72,7 +80,8 @@ function App() {
                     <input id="profession-input" className="popup__input popup__input_type_job" type="text" name="profession" minLength="2" maxLength="200" required />
                     <span className="profession-input-error popup__input-error"></span>
                 </label>
-                <input className="popup__submit" type="submit" name="submit" value="Сохранить"></input></PopupWithForm>
+                <input className="popup__submit" type="submit" name="submit" value="Сохранить"></input>
+        </PopupWithForm>
         <PopupWithForm 
             name="card" 
             title="Новое место" 
@@ -86,7 +95,8 @@ function App() {
                      <input id="link-input" className="popup__input popup__input_type_link" type="url" name="link" placeholder="Ссылка на картинку" />
                      <span className="link-input-error popup__input-error"></span>
                  </label>
-                 <input className="popup__submit" type="submit" name="submit" value="Создать"></input></PopupWithForm> 
+                 <input className="popup__submit" type="submit" name="submit" value="Создать"></input>
+        </PopupWithForm> 
         <PopupWithForm 
             name="avatar" 
             title="Обновить аватар" 
@@ -96,12 +106,13 @@ function App() {
                      <input id="link-input-avatar" className="popup__input popup__input_type_link" type="url" name="link" placeholder="Ссылка на картинку" required />
                      <span className="link-input-avatar-error popup__input-error"></span>
                  </label>
-                 <input className="popup__submit" type="submit" name="submit" value="Сохранить"></input></PopupWithForm>
+                 <input className="popup__submit" type="submit" name="submit" value="Сохранить"></input>
+        </PopupWithForm>
         <ImagePopup
             onOpen={handleCardClick}
             card={selectedCard}
             onClose={closeAllPopups} />
-    </>
+    </UserContext.Provider>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,35 +1,30 @@
+import React from "react";
 import {useEffect, useState} from "react";
 import avatar from "../images/image.jpg";
 import api from "../utils/Api";
 import Card from "../components/Card";
+import {UserContext} from '../contexts/CurrentUserContext';
 
 function Main(props) {
-    const [userName, setUserName] = useState('Жак-Ив Кусто');
-    const [userDescription , setUserDescription] = useState('Исследователь океана');
-    const [userAvatar, setUserAvatar] = useState(avatar);
+    // const [userName, setUserName] = useState('Жак-Ив Кусто');
+    // const [userDescription , setUserDescription] = useState('Исследователь океана');
+    // const [userAvatar, setUserAvatar] = useState(avatar);
     const [cards, setCards] = useState([]);
+    const userData = React.useContext(UserContext);
 
-    function changeName(res) {
-        setUserName(res.name);
-    }
+    // function changeName() {
+    //     setUserName(userData.name);
+    // }
     
-    function changeDescription(res) {
-        setUserDescription(res.about);
-    }
+    // function changeDescription(res) {
+    //     setUserDescription(res.about);
+    // }
     
-    function changeAvatar(res) {
-        setUserAvatar(res.avatar);
-    }
+    // function changeAvatar(res) {
+    //     setUserAvatar(res.avatar);
+    // }
 
     useEffect(()=> {
-        api.getDataUser()
-            .then((res) => {
-                changeName(res);
-                changeDescription(res);
-                changeAvatar(res);
-            })
-            .catch((error) => console.log(`Ошибка при загрузке данных пользователя: ${error}`))
-
         api.getInitialCards()
             .then((cardList) => {
                 setCards([...cards, ...cardList]);
@@ -41,11 +36,11 @@ function Main(props) {
         <main className="main">
             <section className="profile">
                 <div className="profile__change-avatar" onClick={props.onEditAvatar}></div>
-                <img className="profile__avatar" src={userAvatar} alt="Жак-Ив Кусто" />
+                <img className="profile__avatar" src={userData.avatar} alt="Жак-Ив Кусто" />
                 <div className="profile__info">
-                    <h1 className="profile__name">{userName}</h1>
+                    <h1 className="profile__name">{userData.name}</h1>
                     <button className="profile__edit-button" type="button" onClick={props.onEditProfile}></button>
-                    <p className="profile__profession">{userDescription}</p>
+                    <p className="profile__profession">{userData.about}</p>
                 </div>
                 <button className="profile__add-button" type="button" onClick={props.onAddPlace}></button>
             </section>
